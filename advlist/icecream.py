083b6c42ab71d552aaa3c804e5b44f8d3141e5d2
jchensch <john.henschell@usbank.com>
2021-03-09 13:19:16 +0000

    adding earlier versions of my scripts

---
+++ advlist/icecream.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+name = input("What is your Name?")
+icecream = ["flavors", "salty"]
+icecream.append(99)
+print(f"{icecream[2]} {icecream[0]}, and {name} chooses to be {icecream[1]}.")
---
+++ lab_input/johns_first.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+user_input = input("What is your Name?")
+user_input2 = input("What is the day of the week?")
+print("Hello,",user_input +"!", "Happy",user_input2 + "!")
