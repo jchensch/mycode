d257f29fd8a33e9797ce974808c1678ed83111b5
jchensch <john.henschell@usbank.com>
2021-03-10 13:04:11 +0000

    Learning to troubleshoot

--- custif/lettergrade.py
+++ custif/lettergrade.py
@@ -7,7 +7,7 @@ score = float(input("What is your Test Score?"))
 
 # if input value was higher or equal to 90
 if score > 100:
-    print("YOU ARE A CHEATER!!!")
+    message = 'YOU ARE A CHEATER!!!'
 elif score >= 90:
     message = message + 'A'
 elif score >= 80:
@@ -20,3 +20,6 @@ elif score <= 59:
     message = message + 'F'
 print(message)
 
+# I really like this!
+
+# To challenge you a bit more, I think you should try to make cheaters not get the message printed out, and maybe play around with some more output options. Good job!
