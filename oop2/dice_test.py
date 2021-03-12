1436369e813bc26f99071074d3906067bf908e35
jchensch <john.henschell@usbank.com>
2021-03-12 18:17:20 +0000

    Cheating Dice Game Code

---
+++ oop2/dice_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env pythin3
+#call our cheatdice code
+#import the player, cheat_swapper and Cheat_loaded_dice classes
+from cheatdice import Player
+from cheatdice import Cheat_Swapper
+from cheatdice import Cheat_Loaded_Dice
+
+#New classes we made
+cheater1 = Cheat_Swapper()
+cheater2 = Cheat_Loaded_Dice()
+
+cheater1.roll()
+cheater2.roll()
+
+cheater1.cheat()
+cheater2.cheat()
+
+print("Cheater 1 rolled" + str(cheater1.get_dice()))
+print("Cheater 2 rolled" + str(cheater2.get_dice()))
+
+if sum(cheater1.get_dice()) == sum(cheater2.get_dice()):
+  print("Draw!")
+
+elif sum(cheater1.get_dice()) > sum(cheater2.get_dice()):
+  print("Cheater 1 wins!")
+
+else:
+  print("Cheater 2 wins!")
